94c7a36a2a5199704701477b23f65f403f1bd8ad
Jumadil Abdul Rahman Selian <jufal69@gmail.com>
2022-09-29 10:08:08 +0700

    a

--- environments/default.py
+++ environments/default.py
@@ -3,4 +3,4 @@ default = Blueprint('default', __name__)
 
 @default.route('/')
 def redirectKe():
-    return redirect(request.host_url+"/auth/login")
+    return redirect(request.host_url+"auth/login")
